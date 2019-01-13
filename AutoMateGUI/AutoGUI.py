ec39b602d6def5103ae3f01d4fce3a496cec1d88
rohank2002 <rohansantosh.kamat@sjsu.edu>
2019-01-12 22:42:08 -0800

    Automate GUI

---
+++ AutoMateGUI/AutoGUI.py
@@ -0,0 +1,7 @@
+import pyautogui
+pyautogui.click(282,74)
+#print(pyautogui.position())
+#282,74
+
+pyautogui.typewrite("Airplanes")
+pyautogui.typewrite(["enter"])
---
+++ AutoMateGUI/AutocopyText.py
@@ -0,0 +1,4 @@
+import  pyautogui
+pyautogui.click(282,74)
+pyautogui.hotkey("ctrl","a")
+pyautogui.hotkey("ctrl","c")
